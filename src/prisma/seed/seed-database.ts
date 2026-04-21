aa30180105d43b6f9898067c03e3b30ae4fe10fb
Daniel Zipa <danielzipa@outlook.com>
2026-04-21 15:39:44 -0600

    ADDED: seed

---
+++ src/prisma/seed/seed-database.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
+import { initialData } from './seed';
+
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({ adapter });
+
+async function main() {
+  await prisma.reviews.deleteMany();
+  await prisma.user.deleteMany();
+
+  const { reviews } = initialData;
+
+  await prisma.reviews.createMany({
+    data: reviews,
+  });
+
+  console.log('DB sync successful!');
+}
+
+main()
+  .catch(console.error)
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
---
+++ src/prisma/seed/seed.ts
@@ -0,0 +1,167 @@
+interface ReviewSeed {
+  name: string;
+  rating: number;
+  comment: string;
+}
+
+interface SeedData {
+  reviews: ReviewSeed[];
+}
+
+export const initialData: SeedData = {
+  reviews: [
+    {
+      name: "Sarah Bowles",
+      rating: 5,
+      comment:
+        "This is our 3rd time using prowash 365 and they have always done an excellent job, communicate well, and are very professional! We had our gutters cleaned and house pressure washed and I’ve already had two neighbors ask who we used! Will definitely use them again!",
+    },
+    {
+      name: "Natasha Ivy",
+      rating: 5,
+      comment:
+        "Two Words to describe !!!!!!\n" +
+        "(Great Work) my husband noticed David and his brother in the neighborhood. I'm so thankful for the effort and the time they took to clean our home . Great work guys",
+    },
+    {
+      name: "Lori Powers",
+      rating: 5,
+      comment:
+        "They did an excellent job cleaning the inside and outside of our gutters, roof, house, windows, driveway and sidewalk. They accommodated my schedule to get it done before we had a party. Super nice & professional.",
+    },
+    {
+      name: "J.M. VanKleeck",
+      rating: 4,
+      comment:
+        "Used these folks to clean my gutters and to pressure wash my home! They extremely quick and thorough, and do a simply amazing job! The only reason I gave them 4 out of 5 stars is currently they have no electronic billing/payment option. I personally do not use credit cards and I hate using checks. Aside from that they are wonderful! I will use them again, but will make sure to have cash before I call!",
+    },
+    {
+      name: "Pablo Abreu",
+      rating: 5,
+      comment:
+        "I recently had Juan and his team take care of my home with their pressure washing services, and I couldn't be more satisfied! The entire process was seamless from start to finish. Juan is professional, reliable, and pays attention to every detail. My house looks brand new after the cleaning, and I was impressed by the efficiency and care they put into their work. If you're looking for high-quality pressure washing, I highly recommend Juan's company. Great service, great value!",
+    },
+    {
+      name: "Luis Sanchez",
+      rating: 5,
+      comment:
+        "I recently hired ProWash for a pressure washing job, and I couldn’t be happier with the results. They were incredibly professional from start to finish. The team arrived on time, explained the process thoroughly, and got right to work. My driveway and patio look brand new!",
+    },
+    {
+      name: "Robbie Darby",
+      rating: 5,
+      comment:
+        "PRO WASH 365 did a great job cleaning our gutters and pressure washing including our large driveway, walkway, patio, deck and steps again this year. They are professional, reasonable and careful!",
+    },
+    {
+      name: "reign auto detaling",
+      rating: 5,
+      comment:
+        "I highly recommend Prowash360 they are very professional and well knowledgeable they have been helping us with driveway and home pressure washing !! They help me with the gutters now they look like new !!! Definitely we are going to be using this company in the future. Thank you Prowash!",
+    },
+    {
+      name: "German Medina",
+      rating: 5,
+      comment:
+        "They Did a great job for my business and home . I’m really satisfies with the communication and service, no doubt that did something extra for me with out asking, they care more about the customers than just a service thank you guys so much .im really happy with the results definitely recommend them to everyone 100%",
+    },
+    {
+      name: "Nicolas Sanchez",
+      rating: 5,
+      comment:
+        "Son unos Jovenes muy responsables se preocupan porque sus clientes queden totalmente satisfechos con su servicio y siempre estan dispuestos, a hacer un poco mas de lo que se les pide, no dudan en ofrecer toda la ayuda posible. Si buscan una empresa que le de buenos ofresca buen servicio a muy buen precio, no duden en llamarlos LOS MEJORES MUCHAS GRACIAS POR TODO. Juan y Andres Reyes 360 Prowash",
+    },
+    {
+      name: "Rachmonas 4 Life",
+      rating: 5,
+      comment:
+        "I had leaves removed from my gutters the last 3 winters. Excellent communication and satisfactory work. I would recommend!",
+    },
+    {
+      name: "Albert Harrington",
+      rating: 5,
+      comment:
+        "David and Andrew were fast and efficient while pressure washing my siding, walk way, porch and cleaning my gutters. The price for all of this was extremely reasonable.",
+    },
+    {
+      name: "Camilo Garcia",
+      rating: 5,
+      comment:
+        "ProWash The best company to make a good detail to your home, very clean and effective in its work.",
+    },
+    {
+      name: "sandra ortiz",
+      rating: 5,
+      comment:
+        "Their work is impeccable, efficient and their prices are very reasonable. I would definitely hire their services again.",
+    },
+    {
+      name: "Johan Adrián Herrera vargas",
+      rating: 5,
+      comment:
+        "Es la mejor empresa de lavados e interiores y exteriores en Estados Unido, la mejor experiencia que he tenido , realizan todo tipo de cosas varias, 100% recomendado",
+    },
+    {
+      name: "Daniel Diaz",
+      rating: 5,
+      comment:
+        "Me gustó su atención y su buena disposición para mis horarios, todo estuvo impecable y fueron precios muy racionales 🤙🏻 gracias muchachos.",
+    },
+    {
+      name: "Paula Florez",
+      rating: 5,
+      comment: "I’m loving my back patio! Thanks guys. Excellent service 👌🏻",
+    },
+    {
+      name: "leonardo arango",
+      rating: 5,
+      comment:
+        "Excelente servicio , recomendados 100% , grandes profesionales y que carisma para trabajar",
+    },
+    {
+      name: "Teri Block",
+      rating: 5,
+      comment: "Great job on my windows!! I'm very pleased.",
+    },
+    {
+      name: "camilo russi",
+      rating: 5,
+      comment: "Excellent work, highly recommended",
+    },
+    {
+      name: "Hector Álvarez",
+      rating: 5,
+      comment: "Great service 👍🏽💪🏽",
+    },
+    {
+      name: "Carlos Aguirre",
+      rating: 5,
+      comment: "Good service",
+    },
+    {
+      name: "Malek",
+      rating: 5,
+      comment: "",
+    },
+    {
+      name: "Camilo Romero",
+      rating: 5,
+      comment: "",
+    },
+    {
+      name: "Katherine Dagostini",
+      rating: 5,
+      comment: "",
+    },
+    {
+      name: "Carlos Alberto Rojas",
+      rating: 5,
+      comment: "",
+    },
+    {
+      name: "Maritza Betancourt",
+      rating: 5,
+      comment: "",
+    },
+  ],
+};
